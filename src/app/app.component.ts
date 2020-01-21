294358965f4253a188dba86af87874e6a29724d2
calidion <calidion@gmail.com>
2020-01-21 19:17:01 +0800

    init i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { TranslateService, LangChangeEvent } from "@ngx-translate/core";
+import { Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'qiyebao';
+export class AppComponent implements OnInit{
+  title = "qiyebao";
+  supportedLanguages = ["en", "zh"];
+  constructor(
+    private translate: TranslateService,
+    private titleService: Title,
+  ) {
+    translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      console.log('language changed');
+      translate.get("TITLE").subscribe((res: string) => {
+        titleService.setTitle(res);
+      });
+    });
+  }
+  getLocaleString() {
+    let locale = navigator.language;
+    if (this.supportedLanguages.indexOf(locale) !== -1) {
+      return locale;
+    }
+    locale = locale.split("-")[0];
+    if (this.supportedLanguages.indexOf(locale) !== -1) {
+      return locale;
+    }
+    return "en";
+  }
+
+  setLocale(locale: string) {
+    this.translate.setDefaultLang(locale);
+    this.translate.use(locale);
+    localStorage.setItem("locale", locale);
+  }
+  ngOnInit() {
+    let locale = localStorage.getItem("locale");
+    if (!locale) {
+      locale = this.getLocaleString();
+    }
+    this.setLocale(locale);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -13,6 +21,14 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    }),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
