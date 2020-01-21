6e4b00e1903db5471fb3fce733f01a8d4abeb6d6
calidion <calidion@gmail.com>
2020-01-21 21:43:54 +0800

    enabled i18n switching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,39 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, AfterViewInit } from "@angular/core";
 
 import { TranslateService, LangChangeEvent } from "@ngx-translate/core";
 import { Title } from "@angular/platform-browser";
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from './config.service';
 
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  title = "qiyebao";
+export class AppComponent implements OnInit, AfterViewInit {
   supportedLanguages = ["en", "zh"];
+
+  active = 0;
+
+  // Company info
+  name = "NAV-NAME";
+  intro;
+  url = "home";
+  samples;
   constructor(
     private translate: TranslateService,
     private titleService: Title,
+    private http: HttpClient,
+    public config: ConfigService
   ) {
     translate.onLangChange.subscribe((event: LangChangeEvent) => {
       console.log('language changed');
       translate.get("TITLE").subscribe((res: string) => {
         titleService.setTitle(res);
+      });
+      this.config.request("assets/api/intro", (data) => {
+        this.intro = data;
       });
     });
   }
@@ -35,15 +50,41 @@ export class AppComponent implements OnInit{
   }
 
   setLocale(locale: string) {
+    this.config.language = locale;
     this.translate.setDefaultLang(locale);
     this.translate.use(locale);
+    $("select").val(locale);
+    $(".selectpicker").selectpicker("refresh");
     localStorage.setItem("locale", locale);
   }
+
+  ngAfterViewInit() {
+    console.log(this.url);
+
+    $(".carousel").carousel({
+      interval: 10000
+    });
+
+    $(".selectpicker").selectpicker();
+    $(".selectpicker").on("changed.bs.select", e => {
+      console.log("change detected!");
+      console.log(e.target.value);
+      this.setLocale(e.target.value);
+    });
+  }
+
   ngOnInit() {
     let locale = localStorage.getItem("locale");
     if (!locale) {
       locale = this.getLocaleString();
     }
     this.setLocale(locale);
+    this.config.request("assets/api/intro", (data) => {
+      this.intro = data;
+    });
+
+    this.config.request("assets/api/product", (data: any) => {
+      this.samples = data.products;
+    });
   }
 }
